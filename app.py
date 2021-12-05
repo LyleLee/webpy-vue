7ec5c451c24f3827874c253233fc0e149ddf35e4
lixianfa <lixianfa.official@gmail.com>
2021-12-06 00:15:46 +0800

    can get a data from server

--- app.py
+++ app.py
@@ -1,25 +1,46 @@
 import web
+import requests
+import json
 
 urls = (
-    '/','index',
+    '/', 'index',
+    '/api/json/(.*)', 'APIProvider',
     '/(.*)', 'file'
 )
 
 web.config.debug = True
 render = web.template.render('templates/')
 
+
+
+def is_json(myjson):
+  try:
+    json.loads(myjson)
+  except ValueError as e:
+    return False
+  return True
+
 class index:
     def GET(self):
         return render.index()
 
+
 class file:
-    def GET(self,name):
+    def GET(self, name):
         print(name)
         return "Not define this routes yet: " + name
 
 
-if __name__ == "__main__":
+class APIProvider:
+    def GET(self, name):
+        print('APIProvider get api:', name)
+        res = requests.get('https://www3.nhk.or.jp/news/easy/news-list.json').json()
+        return res
 
+#
+
+
+if __name__ == "__main__":
 
     app = web.application(urls, globals())
     app.run()
--- static/js/index.js
+++ static/js/index.js
@@ -5,16 +5,26 @@ $(document).ready(function () {
         data: {
             date: '',
             title: 'initial value',
-            url: ''
+            url: '',
+            info: null
         },
+
         created: function () {
             var _this = this;
             console.log(">>", location.host);
-            console.log(">>",location.protocol)
-            $.getJSON( 'http://'+location.host+'/static/json/news.json', function (json) {
+            console.log(">>", location.protocol)
+            $.getJSON('http://' + location.host + '/static/json/news.json', function (json) {
                 _this.date = json[0]["2021-11-02"][0].news_creation_time;
                 _this.title = json[0]["2021-11-02"][0].title;
                 _this.url = json[0]["2021-11-02"][0].news_web_url;
+            });
+        },
+
+        mounted() {
+            axios.get('http://' + location.host + '/api/json/nhk.json').then(response => {
+                console.log(typeof response.data);
+                this.info = response.data;
+                console.log();
             });
         }
     });
